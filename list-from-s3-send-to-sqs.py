973b375918c518f96ad6f76b0b17ee75d99d8404
kaisasoares <kaisasoares@hotmail.com>
2022-09-14 12:05:16 -0300

    adding files

---
+++ list-from-s3-send-to-sqs.py
@@ -0,0 +1,54 @@
+import boto3
+import json
+import uuid
+
+s3 = boto3.client('s3')
+sqs = boto3.client('sqs')
+
+bucket = 'grupon-solution-sprint3'
+#urlSQS = 'https://sqs.us-east-1.amazonaws.com/873816429411/grupon-queue-csv-to-json'
+#keyprefix = 'files-small/'
+urlSQS = 'https://sqs.us-east-1.amazonaws.com/873816429411/grupon-json-to-firehose'
+keyprefix = 'json/'
+
+def getListofKeyObjects(bucket,keyPrefix):
+
+    response = s3.list_objects_v2(
+        Bucket=bucket,
+        Prefix=keyPrefix
+    )
+
+    listObjectKeys=[]
+    for obj in response["Contents"]:
+        listObjectKeys.append({"bucket":bucket,"key":obj["Key"]})
+        # print(obj["Key"])
+    print(len(listObjectKeys))
+    # print(response)
+    return listObjectKeys
+
+
+def mountMessagesToSQS(listObjectKeys,numberMsgsByBatch=10):
+    listEntriesSQS=[]
+    for obj in listObjectKeys:
+        listEntriesSQS.append(
+            {
+                "Id": str(uuid.uuid1()),
+                'MessageBody': json.dumps(obj)
+            }
+        )   
+    listOfLists = [listEntriesSQS[i * numberMsgsByBatch:(i + 1) * numberMsgsByBatch]
+                   for i in range((len(listEntriesSQS) + numberMsgsByBatch - 1) // numberMsgsByBatch)]
+    return listOfLists
+
+
+def sendToQueue(url, listToSend):
+    response = sqs.send_message_batch(
+        QueueUrl=url,
+        Entries=listToSend
+    )
+    print(response)
+
+listObjectKeys = getListofKeyObjects(bucket,keyprefix)   
+listOfLists = mountMessagesToSQS(listObjectKeys)
+for listToSend in listOfLists:
+    sendToQueue(urlSQS,listToSend)
---
+++ read-sqs-send-to-firehose.py
@@ -0,0 +1,65 @@
+from kinesisHandler import KinesisHandler
+import json
+import uuid
+import time
+import boto3
+
+
+
+kinesis = KinesisHandler('grupon-kns-ingest-json')
+s3 = boto3.resource('s3')
+sqs = boto3.client('sqs')
+urlSQS = 'https://sqs.us-east-1.amazonaws.com/873816429411/grupon-json-to-firehose'
+
+
+def getMessageFromQueue(urlSQS):
+    response = sqs.receive_message(
+        QueueUrl=urlSQS,
+        MaxNumberOfMessages=1
+    )
+    if "Messages" in response:
+        receiptHandle = response["Messages"][0]["ReceiptHandle"]
+        message = response["Messages"][0]["Body"]
+        return receiptHandle, json.loads(message)
+    else:
+        return None, None
+
+
+def deleteMessageFromQueue(urlSQS, receiptHandle):
+    response = sqs.delete_message(
+        QueueUrl=urlSQS,
+        ReceiptHandle=receiptHandle
+    )
+    print("message deleted: " + json.dumps(response))
+
+
+def readObjectAndSendToFirehose(bucket, key):
+    obj = s3.Object(bucket, key)
+    cont = 0
+    listLines = []
+    for line in obj.get()['Body']._raw_stream:
+        # s = StringIO(line.decode("utf-8"))
+        listLines.append(line.decode("utf-8"))
+        if cont == 149:
+            kinesis.put_record(listLines)
+            cont = 0
+            listLines = []
+        else:
+            cont += 1
+    if len(listLines) > 0:
+        kinesis.put_record(listLines)
+        print("enviou fora for")
+
+while True:
+    receiptHandle, message = getMessageFromQueue(urlSQS)
+    if receiptHandle == None:
+        print("acabou a fila")
+        break
+    else:
+        print("received message: " + str(message))
+        bucket = message["bucket"]
+        key = message["key"]
+        readObjectAndSendToFirehose(bucket, key)
+        deleteMessageFromQueue(urlSQS,receiptHandle)
+
+print("terminou com sucesso")
